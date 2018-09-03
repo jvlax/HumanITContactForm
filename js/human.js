eefc99f4ea320696b3498657a209569873527b3d
Johannes Valter-Lax <Johannes.valter-lax@visma.com>
2018-09-03 14:55:10 +0200

    trying to populate winner page thrugh ajax call

--- js/human.js
+++ js/human.js
@@ -4,9 +4,11 @@ function getWinners() {
     cache: false,
     dataType: "text",
     success: function(result) {
-      console.log(result);
       for (var i = 0; i < result.length; i++) {
         console.log(result[i].email);
+        document.getElementById("winnerWrap").innerHTML +=
+          '<div class="winners text-center">' +
+          result[i].name + " " + result[i].company + '</div>';
       }
     }
   });
