445cce4f2193bd5078f1dcbf20ff609214e06c0c
Johannes Valter-Lax <Johannes.valter-lax@visma.com>
2018-09-03 15:05:28 +0200

    forgot to clean some stuff

--- js/human.js
+++ js/human.js
@@ -5,10 +5,7 @@ function getWinners() {
     dataType: "text",
     success: function(result) {
       for (var i = 0; i < result.length; i++) {
-        console.log(result[i].email);
-        document.getElementById("winnerWrap").innerHTML +=
-          '<div class="winners text-center">' +
-          result[i].name + " " + result[i].company + '</div>';
+        document.getElementById("winnerWrap").innerHTML = result;
       }
     }
   });
